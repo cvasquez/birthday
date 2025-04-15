787df2431197cfaaab745927eedaf1446edf8637
Graham Vasquez <graham@gvasquez.dev>
2025-04-14 22:16:53 -0400

    add link to honoree page

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -1,7 +1,22 @@
-import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Box,
+  Menu,
+  IconButton,
+  MenuItem,
+  Link,
+} from '@mui/material';
+import { useState } from 'react';
+import { Link as RouterLink, Outlet } from 'react-router-dom';
+import { useAuth } from 'src/contexts/AuthContext';
+import { AccountCircle } from '@mui/icons-material';
 
 export default function AppLayout() {
+  const { user } = useAuth();
+  const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null);
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <AppBar position="static">
@@ -9,6 +24,24 @@ export default function AppLayout() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Birthday Website Builder
           </Typography>
+          {user && (
+            <>
+              <IconButton color="inherit" onClick={e => setAnchorElement(e.currentTarget)}>
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                open={!!anchorElement}
+                anchorEl={anchorElement}
+                onClose={() => setAnchorElement(null)}
+              >
+                <MenuItem>
+                  <Link component={RouterLink} to="/honorees">
+                    Honorees
+                  </Link>
+                </MenuItem>
+              </Menu>
+            </>
+          )}
         </Toolbar>
       </AppBar>
       <Container component="main" sx={{ mt: 4, mb: 4, flex: 1 }}>
